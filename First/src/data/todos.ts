cfe0cc5def25fc3e16c362a3d0d6d7bc162fdea8
Umang Agarwal <umangagarwal257@gmail.com>
2025-02-14 00:31:51 +0530

    Started React

---
+++ First/src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData : Todo[] = [
+    {
+        id : 1,
+        title : "Like this video 👍",
+        completed : false
+    },
+    {
+        id : 2,
+        title : "Subscribe to My Channel 📺",
+        completed : false
+    },
+    {
+        id : 3,
+        title : "Leave a nice Comment 😀",
+        completed : false
+    },
+]
