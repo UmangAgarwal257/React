635d3a7d5b22c5d9c882d8757f119d6be3cf97c1
Umang Agarwal <umangagarwal257@gmail.com>
2025-02-28 20:33:14 +0530

    Added more features

--- Tenzies/src/components/Die.jsx
+++ Tenzies/src/components/Die.jsx
@@ -1,8 +1,10 @@
 // eslint-disable-next-line react/prop-types
-export default function Die({ value }) {
+export default function Die({ value, isHeld }) {
   return (
     <>
-      <button>{value}</button>
+      <button style={{ backgroundColor: isHeld ? "green" : undefined }}>
+        {value}
+      </button>
     </>
   );
 }
