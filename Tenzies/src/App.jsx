507d6343e1f9f5e7ef513d1db9b5500a02dd3382
Umang Agarwal <umangagarwal257@gmail.com>
2025-03-02 21:24:48 +0530

    Tenzies completed

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,30 +1,64 @@
 import { useState } from "react";
 import Die from "./components/Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice());
+  const [dice, setDice] = useState(() => generateAllNewDice());
+
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
 
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
-      isHeld: true,
+      isHeld: false,
       id: nanoid(),
     }));
   }
+
+  function hold(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
   function rollDice() {
-    setDice(generateAllNewDice());
+    setDice(
+      gameWon
+        ? generateAllNewDice()
+        : (oldDice) =>
+            oldDice.map((die) =>
+              die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+            )
+    );
   }
 
   const diceElements = dice.map((diceObj) => (
-    <Die key={diceObj.id} value={diceObj.value} isHeld={diceObj.isHeld} />
+    <Die
+      key={diceObj.id}
+      value={diceObj.value}
+      isHeld={diceObj.isHeld}
+      hold={hold}
+      id={diceObj.id}
+    />
   ));
 
   return (
     <main>
+      {gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && (
+          <p>Congratulations! You won! Press New Game to start again.</p>
+        )}
+      </div>
+      <h1 className="title">Tenzies</h1>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
-        Roll Dice
+        {gameWon ? "New Game" : "Roll Dice"}
       </button>
     </main>
   );
--- Tenzies/src/components/Die.jsx
+++ Tenzies/src/components/Die.jsx
@@ -1,9 +1,19 @@
-// eslint-disable-next-line react/prop-types
-export default function Die({ value, isHeld }) {
+/* eslint-disable react/prop-types */
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+
   return (
     <>
-      <button style={{ backgroundColor: isHeld ? "lightgreen" : undefined }}>
-        {value}
+      <button
+        onClick={() => props.hold(props.id)}
+        style={styles}
+        aria-pressed={props.isHeld}
+        aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+      >
+        {props.value}
       </button>
     </>
   );
