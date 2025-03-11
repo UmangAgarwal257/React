6e7a31ac1d4bc793ea6d6ca44c25bd8f3c16df6a
Umang Agarwal <umangagarwal257@gmail.com>
2025-03-11 20:33:20 +0530

    Add : useContext hook

--- state-experiments/src/App.jsx
+++ state-experiments/src/App.jsx
@@ -1,7 +1,60 @@
+import { useContext } from "react";
+import { createContext } from "react";
+import { useState } from "react";
+
+const BulbContext = createContext();
+
 function App() {
+  const [bulbOn, setBulbOn] = useState(true);
+
   return (
     <>
-      <div>hi</div>
+      <div>
+        <BulbContext.Provider
+          value={{
+            bulbOn,
+            setBulbOn,
+          }}
+        >
+          <LightBulb />
+        </BulbContext.Provider>
+      </div>
+    </>
+  );
+}
+
+function LightBulb() {
+  return (
+    <>
+      <div>
+        <BulbState />
+        <ToggleBulbState />
+      </div>
+    </>
+  );
+}
+
+function BulbState() {
+  const { bulbOn } = useContext(BulbContext);
+  return (
+    <>
+      <div>{bulbOn ? "Bulb on" : "Bulb off"}</div>
+    </>
+  );
+}
+
+function ToggleBulbState() {
+  const { setBulbOn } = useContext(BulbContext);
+
+  function toggleBulb() {
+    setBulbOn((prev) => !prev);
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={toggleBulb}>Toggle the bulb</button>
+      </div>
     </>
   );
 }
