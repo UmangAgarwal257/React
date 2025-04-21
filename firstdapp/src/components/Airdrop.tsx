60cb528b6a585c44c260ecb85a8c4c14391debf6
Umang Agarwal <umangagarwal257@gmail.com>
2025-04-21 21:34:19 +0530

    airdrop done

--- firstdapp/src/components/Airdrop.tsx
+++ firstdapp/src/components/Airdrop.tsx
@@ -1,9 +1,41 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useState } from "react";
 
 export const Airdrop = () => {
+  
+  const [amount, setAmount] = useState(0);
+  const wallet = useWallet();
+  const {connection} = useConnection();
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value));
+  };
+
+  const sendAirdropToUser = async () => {
+    if (!wallet.publicKey) {
+      console.error("Wallet not connected");
+      return;
+    }
+    await connection.requestAirdrop(
+      wallet.publicKey,
+      amount * LAMPORTS_PER_SOL
+    );
+    alert("Airdrop sent!");
+    const balance = await connection.getBalance(wallet.publicKey); 
+    alert(`New balance: ${balance / LAMPORTS_PER_SOL} SOL`);
+  };
+
+
   return (
     <div>
-      <input type="text" placeholder="Amount" />
-      <button>Send Airdrop</button>
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={handleAmountChange}
+      />
+      <button onClick={sendAirdropToUser}>Send Airdrop</button>
     </div>
   );
 };
